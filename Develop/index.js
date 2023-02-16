fb7b59af10e9f306181e4176cdd9a0cdcd5466ea
apersonius <personius12@gmail.com>
2023-02-15 22:15:54 -0600

    create function to write README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
@@ -7,50 +8,64 @@ const questions = [
     inquirer
     .prompt([
         {
+            type: 'input',
             name: 'projectName',
             message: 'What is the name of your project?'
         },
         {
+            type: 'input',
             name: 'description',
             message: 'Enter a brief description of your project'
         },
         {
+            type: 'input',
             name: 'tableOfContents',
             message: 'Enter a Table of Contents (if applicable) for your project'
         },
         {
+            type: 'input',
             name: 'installation',
             message: 'Enter installation instructions for your project'
         },
         {
+            type: 'input',
             name: 'usage',
             message: 'Enter the usage for your project'
         },
         {
+            type: 'input',
             name: 'license',
             message: 'Enter any licenses for your project'
         },
         {
+            type: 'input',
             name: 'contributers',
             message: 'Enter any contributers for this project'
         },
         {
+            type: 'input',
             name: 'tests',
             message: 'Enter any tests for this project'
         },
         {
+            type: 'input',
             name: 'questions',
-            message: 'Enter any questions for this project'
+            message: 'Contact me with any further questions on github or via email'
         },
         
     ])
-    .then(answers => {
-        console.info('Answer:', answers);
-    })
-];
+    .then((data) => {
+        const filename = `readme`;
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+          err ? console.log(err) : console.log('Success!')
+        );
+      })
+];
+
 
 // TODO: Create a function to initialize app
 function init() {}
