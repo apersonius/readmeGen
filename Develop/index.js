4896055f60956e774f287ac1fafa3c6909c11db7
apersonius <personius12@gmail.com>
2023-02-15 21:16:18 -0600

    install inquirer, create first prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+    .prompt([
+        {
+            name: 'projectName',
+            message: 'What is the name of your project?'
+        },
+    ])
+    .then(answers => {
+        console.info('Answer:', answers.projectName);
+    })
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
