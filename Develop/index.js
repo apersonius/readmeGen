122d74981d5ae018e21cf4ac3475ba4fb6733028
apersonius <personius12@gmail.com>
2023-02-15 21:26:30 -0600

    add all prompts needed per acceptance criteria

--- Develop/index.js
+++ Develop/index.js
@@ -10,9 +10,42 @@ const questions = [
             name: 'projectName',
             message: 'What is the name of your project?'
         },
+        {
+            name: 'description',
+            message: 'Enter a brief description of your project'
+        },
+        {
+            name: 'tableOfContents',
+            message: 'Enter a Table of Contents (if applicable) for your project'
+        },
+        {
+            name: 'installation',
+            message: 'Enter installation instructions for your project'
+        },
+        {
+            name: 'usage',
+            message: 'Enter the usage for your project'
+        },
+        {
+            name: 'license',
+            message: 'Enter any licenses for your project'
+        },
+        {
+            name: 'contributers',
+            message: 'Enter any contributers for this project'
+        },
+        {
+            name: 'tests',
+            message: 'Enter any tests for this project'
+        },
+        {
+            name: 'questions',
+            message: 'Enter any questions for this project'
+        },
+        
     ])
     .then(answers => {
-        console.info('Answer:', answers.projectName);
+        console.info('Answer:', answers);
     })
 ];
 
