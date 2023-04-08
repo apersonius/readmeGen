6bc9a0a40120192509077c6485e2968529590492
apersonius <personius12@gmail.com>
2023-04-08 18:52:44 -0500

    fix bugs again

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -64,11 +64,11 @@ function renderLicenseLink(license) {
   }
 }
 
-const license = `${data.license}`;
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  const license = `${data.license}`;
+
   if (license === "None") {
     return ``;
   } else if (license === "Apache License 2.0") {
