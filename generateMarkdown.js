d1ead98f520ffdbb312d1859bd7506010f5f382c
apersonius <personius12@gmail.com>
2023-04-08 18:55:20 -0500

    fix more errors

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
 
-  if (license === "none") {
+  if (license === "None") {
     return ``;
   } else if (license === "Apache License 2.0") {
     return `![License: Apache License 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
@@ -67,7 +67,6 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  const license = `${data.license}`;
 
   if (license === "None") {
     return ``;
@@ -100,6 +99,7 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const license = `${data.license}`;
   return `# ${data.title}
 \n
 ${renderLicenseBadge(license)}
